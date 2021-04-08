c2cdf84c85e21b887b0e49fd4fa076b0b8cdfba7
hoan-f <shiq9692122@126.com>
2021-04-08 21:56:55 +0800

    Move celery beat runtime files to /tmp

--- dockerfile/celery-beat/setup.sh
+++ dockerfile/celery-beat/setup.sh
@@ -4,16 +4,19 @@ proj_path="$1"
 proj_name=`basename $proj_path .py`
 work_dir=`dirname $proj_path`
 
+supervisord_conf=/tmp/supervisord.conf
+schedule_db=/tmp/celerybeat-schedule
+
 cd $work_dir || exit 1
 
-cat >supervisord.conf <<EOF
+cat >$supervisord_conf <<EOF
 [supervisord]
 nodaemon=true
 logfile=/tmp/supervisord.log
 pidfile=/tmp/supervisord.pid
 
 [program:celery]
-command=/usr/bin/celery -A $proj_name beat
+command=/usr/bin/celery -A $proj_name beat -s "$schedule_db" --pidfile=/tmp/celerybeat.pid
 EOF
 
-exec /usr/bin/supervisord
+exec /usr/bin/supervisord -c $supervisord_conf
